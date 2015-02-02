bd21b51983bb704fdacd9b1fca926cd3f0a259ea
Samy Pessé <samypesse@gmail.com>
2015-02-02 16:42:00 +0100

    Add text and facebook tile

--- convert.sh
+++ convert.sh
@@ -8,11 +8,10 @@ fi
 # Clean pngs folder
 rm -rf ./pngs/*
 
-# Convert to png equivalent
-svgexport ./src/apple-touch-icon.svg ./pngs/apple-touch-icon-152.png 152:152
+## Logos with text
+svgexport ./src/gitbook-text.svg ./pngs/512-text.png 512:102
 
-svgexport ./src/published-with-gitbook.svg ./pngs/published-with-gitbook.png
-
+## Logos
 svgexport ./src/gitbook-white.svg ./pngs/512wt.png
 
 svgexport ./src/gitbook-transparent.svg ./pngs/512t.png
@@ -24,4 +23,14 @@ svgexport ./src/gitbook.svg ./pngs/128.png 128:128
 svgexport ./src/gitbook.svg ./pngs/64.png 64:64
 svgexport ./src/gitbook.svg ./pngs/32.png 32:32
 svgexport ./src/gitbook.svg ./pngs/16.png 16:16
+
+## Apple
+svgexport ./src/apple-touch-icon.svg ./pngs/apple-touch-icon-152.png 152:152
+
+## Facebook
+svgexport ./src/facebook-tile.svg ./pngs/tile-facebook.png 1200x630
+
+## Book cover
+svgexport ./src/published-with-gitbook.svg ./pngs/published-with-gitbook.png
+
 
